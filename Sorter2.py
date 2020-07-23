7645f9bf27c4f221ff13bb5c29c541e407c08498
Parsa Hassani <mohamad.parsa.hasani@gmail.com>
2020-07-23 10:42:38 -0700

    Changed script so that multiple sheets are added

--- Sorter2.py
+++ Sorter2.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import os
+import numpy as np
 
 
 
 all_df_list = []
+
 cwd = os.path.abspath('')
 files = os.listdir(cwd)
+writer = pd.ExcelWriter(cwd + '/Sorted_Stocks.xlsx', engine = 'xlsxwriter')
+counter = 0
 
 for file in files:
     if file.endswith ('.csv'):
         all_df_list.append(pd.read_csv(file))
+        if(len(all_df_list)):
+            counter = counter + 1
+            appended_df = pd.concat(all_df_list)
+            appended_df.to_excel(writer, sheet_name = 'Set' + str(counter))
+            all_df_list = []
 
+writer.save()
+writer.close()
 
-appended_df = pd.concat(all_df_list)
-
-appended_df.to_excel("Sorted_Stocks.xlsx", index=False)
+#appended_df.to_excel("Sorted_Stocks.xlsx", index=False)
